23db7af31dfa04d3704890f594eeaf201bd772ea
07rauf <raufn759@gmail.com>
2024-04-30 21:14:10 +0400

    reactinput

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,27 @@ import pizza3 from './img/pizza3.png';
 
 function App() {
     const [missions, setMissions] = useState([
-        { name: "Italian Pizza", price: 10, id: 1, image: pizza1, isVisible: true },
-        { name: "Hawaiian Pizza", price: 12, id: 2, image: pizza2, isVisible: true },
-        { name: "Slice Pizza", price: 8, id: 3, image: pizza3, isVisible: true }
+        { name: "Italian Pizza", price: 10, id: 1, image: pizza1, isVisible: true, clickCount: 0 },
+        { name: "Hawaiian Pizza", price: 12, id: 2, image: pizza2, isVisible: true, clickCount: 0 },
+        { name: "Slice Pizza", price: 8, id: 3, image: pizza3, isVisible: true, clickCount: 0 }
     ]);
 
-    const [visibleImages, setVisibleImages] = useState([]);
     const [totalPrice, setTotalPrice] = useState(0); 
     const [isDivVisible, setIsDivVisible] = useState(false);
 
     const handleAddTask = (id, price) => {
         const clickedMission = missions.find(mission => mission.id === id);
         if (clickedMission) {
-            setVisibleImages(prev => [...prev, clickedMission]);
-            setTotalPrice(prev => prev + price); 
+            setTotalPrice(prevPrice => prevPrice + price); 
+            setMissions(prevMissions => 
+                prevMissions.map(mission => 
+                    mission.id === id ? {...mission, clickCount: mission.clickCount + 1} : mission
+                )
+            ); 
         }
     };
 
-    const calculateTotalPrice = () => {
-        return totalPrice;
-    };
-
-    const DivVisibility = () => {
+    const toggleDivVisibility = () => {
         setIsDivVisible(prev => !prev);
     };
 
@@ -45,13 +44,17 @@ function App() {
             <br />
             {isDivVisible && (
                 <div className='divmain'>
-                    {visibleImages.map((image, index) => (
-                        <img className='divmainimg' key={index} src={image.image} alt={`pizza ${image.id}`} />
+                    {missions.map(mission => (
+                        mission.clickCount > 0 &&
+                        <div key={mission.id}>
+                            <img className='divmainimg' src={mission.image} alt={`pizza ${mission.id}`} />
+                            <div>Neçə ədəd sifariş etmisiniz: {mission.clickCount}</div> 
+                        </div>
                     ))}
-                    <div>Total Price: ${calculateTotalPrice()}</div>
+                    <div className='total'>Total Price: ${totalPrice}</div>
                 </div>
             )}
-            <button className='hidebtn' onClick={DivVisibility}>+</button>
+            <button className='hidebtn' onClick={toggleDivVisibility}>+</button>
         </div>
     );
 }
